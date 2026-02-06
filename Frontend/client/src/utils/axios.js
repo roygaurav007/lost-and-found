435364f30b5e1ea722f5ae4bc4e50a2db83f35f1
Mishrasujal <sujalvats01@gmail.com>
2026-02-07 00:32:25 +0530

    Backend ready for deployment

--- Frontend/client/src/utils/axios.js
+++ Frontend/client/src/utils/axios.js
@@ -1,19 +1,22 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-// Attach token automatically
-axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("findit_token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// Attach JWT token automatically
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("findit_token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
